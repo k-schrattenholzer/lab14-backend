f13257ffd0cab0e0cb4996a24e7b7627a4603fec
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-10-13 16:54:02 -0700

    initial data setup and heroku created

---
+++ data/favorite-books.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: 'books name 1',
+    author: 'author 1',
+    isbn: 1
+  },
+  {
+    title: 'book name 2',
+    author: 'author 2',
+    isbn: 2
+  },
+  {
+    title: 'book name 3',
+    author: 'author 3',
+    isbn: 3
+  },
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const favorite_books = require('./favorite-books.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 run();
@@ -26,12 +26,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorite_books.map(book => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO favorite_books (title, author, isbn, owner_id)
+                    VALUES ($1, $2, $3, $4);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [book.title, book.author, book.isbn, user.id]);
       })
     );
     
